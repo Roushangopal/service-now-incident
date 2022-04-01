d14e56fc05a90fcb60bd57bb4bb038124c2c4bce
Roushan Raja <roushanraja0@gmail.com>
2022-04-01 19:18:55 +0530

    minor

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,10 +1,12 @@
 module.exports = (req, res, next) => {
+    console.log('Authenticating')
     clusterId = [1486794073761400]
     try {
-        if(clusterId.includes(req.body.clusterId)) {
+        if(clusterId.includes(parseInt(req.body.clusterId))) {
             console.log("Authentication Successful");
             next();
         } else {
+            console.log('Auth Failed')
             throw Error("Auth Failed");
         }
     } catch (e) {
